7cf14d37ed8a57647e7b197b76b09cdcd6a8edaa
Veronika Hryhoriv <grigorivveronika@gmail.com>
2023-10-03 10:28:09 +0300

    dev: Change files

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -3,14 +3,6 @@ import { PrismaService } from '../prisma.service';
 import { Books, Prisma } from '@prisma/client';
 // import { Logger, Injectable } from '@nestjs/common';
 
-//1. Logger
-//2. Genres model CRUD
-// Ask about DTO
-
-//3. User Auth registr token login
-//4.boookGenres name fields
-//
-
 @Injectable()
 export class BookService {
   constructor(private prisma: PrismaService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MyLogger } from './logger/logger.service';
+import { ValidationPipe } from '@nestjs/common';
 
 (async function () {
   const app = await NestFactory.create(AppModule, {
     logger: new MyLogger(),
   });
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 })();
